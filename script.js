2a801e1b5c1d952c49a524f5372e76a56105733b
Carlos Eduardo <102683309+carloseduardo-silva@users.noreply.github.com>
2022-08-01 11:38:29 -0300

    ajustando 3

--- script.js
+++ script.js
@@ -7,14 +7,17 @@ function marcar(){
 
     var corte1 = document.getElementById('corte')
     sessionStorage.setItem('corte', corte1.value)
-    corte1.innerHTML = ' '
+    document.getElementById("corte").style.display="none";
+    
 
     var data1 = document.getElementById('dia')
     sessionStorage.setItem('dia', data1.value)
-    data1.removeAttribute 
+    document.getElementById("dia").style.display="none";
+    
 
     var hora1 = document.getElementById('hora')
     sessionStorage.setItem('hora', hora1.value)
+    document.getElementById("hora").style.display="none";
     
 
     resp.innerHTML += `<h1 id="lado2">  Seu horario para ${sessionStorage.getItem('corte')} </h1>`
