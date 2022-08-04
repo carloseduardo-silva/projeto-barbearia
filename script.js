eff70f6235db94c559d56ea8825c70fd840cd06f
Carlos Eduardo <102683309+carloseduardo-silva@users.noreply.github.com>
2022-08-04 13:26:59 -0300

    ajuste final 100%responsivo

--- script.js
+++ script.js
@@ -1,41 +1,27 @@
 
-function marcar(){
-    resp = document.getElementById('degrade')
-    resp.innerHTML = '<h1 id="lado"> Agendamento Concluído! </h1> <br> <h1 id="lado1">Agradece pela preferencia. </h1>  '
-
-    
-
-    var corte1 = document.getElementById('corte')
-    sessionStorage.setItem('corte', corte1.value)
-    document.getElementById("corte").style.display="none";
-    
-
-    var data1 = document.getElementById('dia')
-    sessionStorage.setItem('dia', data1.value)
-    document.getElementById("dia").style.display="none";
-    
-
-    var hora1 = document.getElementById('hora')
-    sessionStorage.setItem('hora', hora1.value)
-    document.getElementById("hora").style.display="none";
-    
-
-    resp.innerHTML += `<h1 id="lado2">  Seu horario para ${sessionStorage.getItem('corte')} </h1>`
-    resp.innerHTML += `<h1 id="lado2"> em ${sessionStorage.getItem('dia')} - ${sessionStorage.getItem('hora')} esta marcado. </h1>`
-    
-}
-
-
-/* 
-function concluido() {
+    function concluido() {
         var cortereal = document.getElementById('corte')
         sessionStorage.setItem('corte', cortereal.value)
-        var limpa = document.getElementById('resp')
-        limpa.style.display='none';
-
+    
         var volta = document.getElementById('resposta')
         volta.innerHTML = ` Horario marcado para ${sessionStorage.getItem('corte')}. `
-    }
+
+        
+        var datareal = document.getElementById('dia')
+        sessionStorage.setItem('dia', datareal.value)
+        document.getElementById("dia").style.display="none";
+        
+        
+        var horareal = document.getElementById('hora')
+        sessionStorage.setItem('hora', horareal.value)
+        document.getElementById("hora").style.display="none";
+        
+        resp = document.getElementById('degrade')
+        resp.innerHTML = '<h1 id="lado"> Agendamento Concluído! </h1> <br> <h1 id="lado1">Agradece pela preferencia. </h1>  '
+
+        resp.innerHTML += `<h1 id="lado2">  Seu horario para ${sessionStorage.getItem('corte')} </h1>`
+        resp.innerHTML += `<h1 id="lado2"> em ${sessionStorage.getItem('dia')} - ${sessionStorage.getItem('hora')} esta marcado. </h1>`
     
-    testar essa function com output, direcionando os inputs corte, dia e hora para dentro da div flex degrade e alinhando a responsividade
-    */
+    
+    }
+
