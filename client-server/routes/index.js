705784bd77fbb57e49c2605f4efb1326c6f51c09
Carlos Eduardo <102683309+carloseduardo-silva@users.noreply.github.com>
2023-06-27 23:38:18 -0300

    ok 27/06/2023
    HTML+Boostrap funcionando no localhost 3000 == pasta view do expresG
    
    pasta public another components

--- client-server/routes/index.js
+++ client-server/routes/index.js
@@ -6,4 +6,30 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/haircuts.html', function(req, res, next) {
+  res.render('haircuts', { title: 'Express' });
+});
+
+router.get('/about.html', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+});
+
+router.get('/agendamento/haircutType.html', function(req, res, next) {
+  res.render('agendamento/haircutType', { title: 'Express' });
+});
+
+router.get('/agendamento/time.html', function(req, res, next) {
+  res.render('agendamento/time', { title: 'Express' });
+});
+
+router.get('/agendamento/clientData.html', function(req, res, next) {
+  res.render('agendamento/clientData', { title: 'Express' });
+});
+
+
+router.get('/agendamento/confirmed.html', function(req, res, next) {
+  res.render('agendamento/confirmed', { title: 'Express' });
+});
+
+
 module.exports = router;
