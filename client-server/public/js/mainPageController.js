9d263c78f80bf2d3fd6915ca459ffca9cb1f9a01
Carlos Eduardo <102683309+carloseduardo-silva@users.noreply.github.com>
2023-07-12 00:17:06 -0300

    ok
    section galeria fotos + pagina de agendamento styles
    + controllers

---
+++ client-server/public/js/mainPageController.js
@@ -0,0 +1,6 @@
+let sectionCortesEl = document.querySelector("#cortes")
+
+let images = document.querySelectorAll('#cortes img')
+
+let proximoBtn = document.querySelectorAll('#proxima-foto')
+let anteriorBtn = document.querySelectorAll('#anterior-foto')
