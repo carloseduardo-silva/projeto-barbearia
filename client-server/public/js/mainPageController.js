67854fa119095e782852d4ddcba5914f279f5c40
Carlos Eduardo <102683309+carloseduardo-silva@users.noreply.github.com>
2023-07-12 12:15:26 -0300

    ok 12/07
    script galeria in working

--- client-server/public/js/mainPageController.js
+++ client-server/public/js/mainPageController.js
@@ -1,6 +1,56 @@
-let sectionCortesEl = document.querySelector("#cortes")
+class mainPageController{
 
-let images = document.querySelectorAll('#cortes img')
+    constructor(){
 
-let proximoBtn = document.querySelectorAll('#proxima-foto')
-let anteriorBtn = document.querySelectorAll('#anterior-foto')
+        this.sectionCortesEl = document.querySelector("#cortes")
+        this.images = document.querySelectorAll('#cortes img')
+        this.proximoBtn = document.querySelector ('#proxima-foto')
+        this.anteriorBtn = document.querySelector('#anterior-foto')
+
+
+        this.initEvents();
+    }
+
+
+    initEvents(){
+
+        
+
+        this.proximoBtn.addEventListener('click', e=>{
+
+            let i = 0 
+
+            this.images.forEach(e =>{
+
+            if(i == (this.images.length -1)){
+                this.images[i].classList.remove('selected')
+                this.images[i].classList.add('hide')
+
+                this.images[0].classList.remove('hide')
+                this.images[0].classList.add('selected')
+            }
+
+            if(e.classList.value ==='selected'){
+
+                this.images[i].classList.remove('selected')
+                this.images[i].classList.add('hide')
+                
+                this.images[i+1].classList.remove('hide')
+                this.images[i+1].classList.add('selected')
+            }
+            else{i++}
+            
+        })
+
+
+        }
+        )   
+    }
+
+    nextPhoto(){
+        
+    }
+}
+
+
+
